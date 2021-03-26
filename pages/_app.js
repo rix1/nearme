b7fbf9b35723c63035c75227f6337546dcced9df
Rikard Eide <rikardeide@gmail.com>
2021-03-27 00:52:13 +0100

    app: remove framer-motion as its unused

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,8 @@
-import React from "react";
-import { AnimatePresence } from "framer-motion";
-import "../styles/index.css";
+import React from 'react';
+import '../styles/index.css';
 
-function MyApp({ Component, pageProps, router }) {
-  return (
-    <AnimatePresence exitBeforeEnter>
-      <Component {...pageProps} key={router.route} />
-    </AnimatePresence>
-  );
+function NearmeRoot({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default NearmeRoot;
