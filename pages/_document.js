4a25dac617571f1bc85acb735727575db1700e6a
Rikard Eide <rikardeide@gmail.com>
2021-03-29 00:10:21 +0200

    analytics: add splitbee

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en-GB">
+        <Head>
+          <script async data-api="/_hive" src="/bee.js" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
