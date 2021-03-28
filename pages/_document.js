d001dc7ba94b213e20be799984a7818385fce549
Rikard Eide <rikardeide@gmail.com>
2021-03-29 00:51:17 +0200

    add favicons

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
@@ -5,6 +6,8 @@ class MyDocument extends Document {
     return (
       <Html lang="en-GB">
         <Head>
+          <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" href="/icon.png" />
           <script async data-api="/_hive" src="/bee.js" />
         </Head>
         <body>
